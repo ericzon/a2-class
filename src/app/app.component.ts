c88e20fb4351f46064299be0245795733c99a2f0
Eric Lara <ericzon@gmail.com>
2017-05-18 19:31:56 +0200

    ADD list filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import  { User } from './modules/user/models/user.interface';
 
 
@@ -7,11 +7,44 @@ import  { User } from './modules/user/models/user.interface';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app works Eric!!';
+  allUsers: User[];
   users: User[];
+  
+  ngOnInit() {
+    this.allUsers = [{
+      name: "Eric",
+      id: 1,
+      active: true
+    }, {
+      name: "Ivan",
+      id: 2,
+      active: true
+    }, {
+      name: "Joan",
+      id: 3,
+      active: true
+    }, {
+      name: "Camilo",
+      id: 4,
+      active: true
+    }, {
+      name: "Karla",
+      id: 5,
+      active: true
+    }, {
+      name: "Konstan",
+      id: 6,
+      active: true
+    }];
+    this.users = this.allUsers;
+  }
 
   searchUser(value: string) {
     console.log("Recibido! searchUser: ",value);
+    this.users = this.allUsers.filter((user) => {
+      return user.name.toLowerCase().indexOf(value.toLowerCase()) !== -1;
+    });
   }
 }
