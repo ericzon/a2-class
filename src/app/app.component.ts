14dc39e8d064ae1a9352c3060e29f15f2030171d
Eric Lara <ericzon@gmail.com>
2017-05-18 18:53:40 +0200

    ADD searchUser on app-input-search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works Eric!!';
+
+  searchUser(value: string) {
+    console.log(value);
+  }
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 import { CommonModule } from '@angular/common';
 import { InputSearchComponent } from './form/input-search/input-search.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgForm
   ],
   declarations: [InputSearchComponent],
   exports: [InputSearchComponent]
